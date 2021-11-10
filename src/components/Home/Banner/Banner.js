14ef8360cd31795118e23c5323352c9ffd6b3fed
TakiKauser <takikauserbadhon@gmail.com>
2021-11-11 01:03:45 +0600

    banner

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -1,10 +1,98 @@
-import React from 'react';
+import * as React from 'react';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MobileStepper from '@mui/material/MobileStepper';
+import Button from '@mui/material/Button';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+const images = [
+    {
+        imgPath:
+            'https://i.ibb.co/R70dm1L/photo-1478679998273-573b05e79e0c.jpg'
+    },
+    {
+        imgPath:
+            'https://i.ibb.co/92N5ybX/photo-1470347051932-8ca24b11b3e3.jpg'
+    },
+    {
+        imgPath:
+            'https://i.ibb.co/N222bHV/photo-1531608139434-1912ae0713cd.jpg'
+    },
+    {
+        imgPath:
+            'https://i.ibb.co/zZvsq2H/photo-1561642445-b789b9a7e6f0.jpg'
+    }
+];
 
 const Banner = () => {
+    const theme = useTheme();
+    const [activeStep, setActiveStep] = React.useState(0);
+    const maxSteps = images.length;
+
+    const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    };
+
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+
+    const handleStepChange = (step) => {
+        setActiveStep(step);
+    };
+
     return (
-        <div>
-            <h2>Banner</h2>
-        </div>
+        <Box sx={{ maxWidth: "100%", flexGrow: 1 }}>
+            <AutoPlaySwipeableViews
+                axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+                index={activeStep}
+                onChangeIndex={handleStepChange}
+                enableMouseEvents
+            >
+                {
+                    images.map((step, index) => (
+                        <div key={step.label}>
+                            {Math.abs(activeStep - index) <= 2 ? (
+                                <Box
+                                    component="img"
+                                    sx={{
+                                        height: 600,
+                                        display: 'block',
+                                        maxWidth: '100%',
+                                        overflow: 'hidden',
+                                        width: '100%',
+                                    }}
+                                    src={step.imgPath}
+                                    alt={step.label}
+                                />
+                            ) : null}
+                        </div>
+                    ))
+                }
+            </AutoPlaySwipeableViews>
+            <MobileStepper
+                steps={maxSteps}
+                position="static"
+                activeStep={activeStep}
+                nextButton={
+                    <Button
+                        size="small"
+                        onClick={handleNext}
+                        disabled={activeStep === maxSteps - 1}
+                    ></Button>
+                }
+                backButton={
+                    <Button
+                        size="small"
+                        onClick={handleBack}
+                        disabled={activeStep === 0}
+                    ></Button>
+                }
+            />
+        </Box>
     );
 };
 
